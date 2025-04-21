7bf167328625cf76ed06f42feb5ffec736e4b644
Adarsh <126099267+Addy20021@users.noreply.github.com>
2025-04-21 15:10:25 +0530

    Add files via upload

---
+++ Productmain.java
@@ -0,0 +1,65 @@
+public class Productmain {
+	 public static void main(String[] args) {
+		  
+			Electronic ec = new Electronic("iphone",150000,"Apple",5);
+			Cloth c = new Cloth("Shirt",1000,"XL","Cotton");
+			System.out.println("Electronic Product:");
+			ec.display();
+			System.out.println("Cloth Product:");
+			c.display();
+			 
+			
+	       
+	       
+	       
+		}
+}
+ class Product {
+	String name;
+	double price;
+	Product(String name,double price){
+		this.name=name;
+		this.price=price;
+	}
+	void display() {
+		System.out.println(name);
+		System.out.println(price);
+	}
+
+}
+ class Electronic extends Product{
+	String Brand;
+	int warrenty;
+	Electronic(String name,double price,String Brand,int warrenty){
+		super(name,price);
+		
+		this.Brand=Brand;
+		this.warrenty=warrenty;
+	}
+	void display() {
+		System.out.println("Name:" + name);
+		System.out.println("Price:"+ price);
+		System.out.println("Brand:"+ Brand);
+		System.out.println("Warrenty:"+warrenty);
+	}
+	
+}
+ class Cloth extends Product{
+	 String size;
+	 String material;
+	 Cloth(String name,double price,String size,String material)
+	 {
+		 super(name,price);
+		 
+		 this.size=size;
+		 this.material=material;
+		 
+	 }
+	 void display() {
+			System.out.println("name:"+name);
+			System.out.println("price:"+price);
+			System.out.println("size:"+size);
+			System.out.println("material:"+material);
+		}
+	 
+ }
